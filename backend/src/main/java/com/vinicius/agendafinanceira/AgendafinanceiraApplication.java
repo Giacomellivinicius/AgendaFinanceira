093836cd1e031e13ada71add0cc10f626a7b3711
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-01-20 08:57:43 -0300

    Projeto criado

---
+++ backend/src/main/java/com/vinicius/agendafinanceira/AgendafinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.agendafinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendafinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendafinanceiraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vinicius/agendafinanceira/AgendafinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.agendafinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendafinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
