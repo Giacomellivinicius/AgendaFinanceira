eb2b9d2d5790ece725cccd5c2dc6ec454cf90edd
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-01-22 11:34:06 -0300

    Http.DELETE para empresas, tratamento de erro DataIntegrityViolation

--- backend/src/main/java/com/vinicius/agendafinanceira/resources/EmpresaResource.java
+++ backend/src/main/java/com/vinicius/agendafinanceira/resources/EmpresaResource.java
@@ -42,6 +42,13 @@ public class EmpresaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id ){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
 	
 
 }
--- backend/src/main/java/com/vinicius/agendafinanceira/services/EmpresaService.java
+++ backend/src/main/java/com/vinicius/agendafinanceira/services/EmpresaService.java
@@ -3,10 +3,12 @@ package com.vinicius.agendafinanceira.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.vinicius.agendafinanceira.domain.Empresa;
 import com.vinicius.agendafinanceira.repositories.EmpresaRepository;
+import com.vinicius.agendafinanceira.services.exceptions.DataIntegrityException;
 import com.vinicius.agendafinanceira.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class EmpresaService {
 	public Empresa update(Empresa obj){
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma empresa que possua contas cadastradas");
+		}
 	}
 
 }
