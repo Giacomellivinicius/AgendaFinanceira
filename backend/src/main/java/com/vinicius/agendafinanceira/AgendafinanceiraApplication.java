ff42dd991ffaaafd4c360b85190cd49b6da4ab1b
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-01-23 21:52:07 -0300

    Perfil dev, conexao PostgreSQL

--- backend/src/main/java/com/vinicius/agendafinanceira/AgendafinanceiraApplication.java
+++ backend/src/main/java/com/vinicius/agendafinanceira/AgendafinanceiraApplication.java
@@ -19,7 +19,8 @@ import com.vinicius.agendafinanceira.repositories.ContaRepository;
 import com.vinicius.agendafinanceira.repositories.EmpresaRepository;
 
 @SpringBootApplication
-public class AgendafinanceiraApplication  implements CommandLineRunner{
+public class AgendafinanceiraApplication implements CommandLineRunner{
+	
 	
 	@Autowired
 	private EmpresaRepository empresaRepository;
@@ -31,45 +32,35 @@ public class AgendafinanceiraApplication  implements CommandLineRunner{
 	private ComprovanteRepository comprovanteRepository;
 	
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm"); 
-
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendafinanceiraApplication.class, args);
 	}
 
+	
 	@Override
 	public void run(String... args) throws Exception {
 
 		Empresa emp1 = new Empresa(null, "Teste", Prioridade.DESEJAVEL);
-		Empresa emp2 = new Empresa(null, "Teste2", Prioridade.DISPENSAVEL);
 		
 		Conta c1 = new Conta(null, null, 10.00, sdf.parse("15/01/1991 11:00"),
 				TipoCusto.CUSTOFIXO,EstadoPagamento.PAGO, null);
-		Conta c2 = new Conta(null, null, 22.00, sdf.parse("15/01/1992 22:00"),
-				TipoCusto.CUSTOVARIAVEL,EstadoPagamento.PENDENTE, null);
-		Conta c3 = new Conta(null, null, 33.00, sdf.parse("15/01/1992 22:00"),
-				TipoCusto.CUSTOVARIAVEL,EstadoPagamento.PAGO, null);
 		
-		Comprovante comp1 = new Comprovante(null, null, sdf.parse("15/02/1922 11:01"));
-		Comprovante comp2 = new Comprovante(null, null, sdf.parse("15/02/1922 12:01"));
+		Comprovante comp1 = new Comprovante(null, null, sdf.parse("15/01/1991 11:01"));
 		
 		c1.setComprovante(comp1);
 		comp1.setConta(c1);
-		
-		c3.setComprovante(comp2);
-		comp2.setConta(c3);
-		
+				
 		emp1.getContas().addAll(Arrays.asList(c1));
-		emp2.getContas().addAll(Arrays.asList(c2,c3));
-		
+				
 		c1.setEmpresa(emp1);
-		c2.setEmpresa(emp2);
-		c3.setEmpresa(emp2);
 		
-		empresaRepository.saveAll(Arrays.asList(emp1,emp2));
-		contaRepository.saveAll(Arrays.asList(c1,c2));
-		comprovanteRepository.saveAll(Arrays.asList(comp1,comp2));
+		empresaRepository.saveAll(Arrays.asList(emp1));
+		contaRepository.saveAll(Arrays.asList(c1));
+		comprovanteRepository.saveAll(Arrays.asList(comp1));
 		
 	}
+	
 
 }
